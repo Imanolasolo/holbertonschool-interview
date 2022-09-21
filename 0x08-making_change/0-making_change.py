004883dd6042f8420352aa5ea2e8bb9c223625c5
Imanolasolo <jjusturi@gmail.com>
2022-09-21 10:02:05 -0500

    0 exercise

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-"""Making change O(n)"""
+"""
+a python module to make a change
+"""
 
 
 def makeChange(coins, total):
-    """Clasic Bottom-Up dynamic programming"""
-    temp_value = 0
-    coins.sort(reverse=True)
-
+    """
+    makeChange-function to find fewest number of coins needed to meet total
+    Arguments:
+        coins: the given coins
+        total: the given total we need to acheive
+    Returns:
+        the fewest number of coins to meet total
+    """
     if total <= 0:
         return 0
-
-    for coin in coins:
-        if total % coin <= total:
-            temp_value += total // coin
-            total = total % coin
-
-    return temp_value if total == 0 else -1
+    sorted_coins = sorted(coins, reverse=True)
+    coins_nbr = {}
+    for cn in sorted_coins:
+        if total % cn == 0:
+            coins_nbr[cn] = total / cn
+            rslt = sum(coins_nbr.values())
+            return int(rslt)
+        else:
+            coins_nbr[cn] = total // cn
+            total = total % cn
+    return -1
